81c4d75bb7d1b6c857569751b03675359acdb56c
muhammadikrama9@outlook.com <muhammadikrama9@outlook.com>
2021-08-23 06:43:32 -0700

    Add new project.

---
+++ BasicOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
